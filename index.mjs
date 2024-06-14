f3ee546737f8d3cbefaf92e28ce916890097f8c4
Alexandre Abreu Ferreira <ale7java@gmail.com>
2024-06-14 11:37:17 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,14 +1,25 @@
 import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
-export const handler = async(event) => {
+const s3Client = new S3({ region: 'us-east-1' });
+// Código omitido
 
-    log('event :' + JSON.stringify(event));
+export const handler = async(event, context, callback) =>{
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
 
-    return {
-        statusCode: 200,
-        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
-        headers: {
-            "content-type": "text/html"
-        }
-    };
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
